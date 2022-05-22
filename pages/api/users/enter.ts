42bf1d94dac0b2eb26af2bb72b4ebd707f1146ad
tars <qnfmtm666@naver.com>
2022-05-22 16:18:13 +0900

    enter.ts phone 타입 수정

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -9,7 +9,7 @@ import { client } from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const user = phone ? { phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
